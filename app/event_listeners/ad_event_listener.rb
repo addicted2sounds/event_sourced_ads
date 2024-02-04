ffcb560e8951f338b59aa41a038637f203534d38
Oleg Borys <addicted2sounds@gmail.com>
2024-02-04 12:08:47 +0200

    fix event listener

--- app/event_listeners/ad_event_listener.rb
+++ app/event_listeners/ad_event_listener.rb
@@ -1,7 +1,7 @@
 class AdEventListener < ApplicationEventListener
   class << self
     def apply_ad_created(data:, stream_name:)
-      Ad.create!(id: stream_name, **data)
+      Ad.create!(id: stream_name, **data.slice(:title, :body))
     end
   end
 end
